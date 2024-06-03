2112583dda9adeb2834e054d5ac01da329169ac1
minani <1989015@pcu.ac.kr>
2024-06-03 20:30:32 +0900

    목표관리 창 디자인 수정 및 캘린더 추가

---
+++ JavaScript/calendarScript.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        editable: true,
+        selectable: true,
+        selectHelper: true,
+        events: [],
+        select: function(start, end) {
+            var title = prompt('Event Title:');
+            var eventData;
+            if (title) {
+                eventData = {
+                    title: title,
+                    start: start,
+                    end: end
+                };
+                $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+            }
+            $('#calendar').fullCalendar('unselect');
+        },
+        eventClick: function(event) {
+            var isConfirmed = confirm('Do you really want to delete this event?');
+            if (isConfirmed) {
+                $('#calendar').fullCalendar('removeEvents', event._id);
+            }
+        }
+    });
+});
